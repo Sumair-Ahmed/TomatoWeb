3695b3a1c520516b34f55eb6e8f5aa9769695aad
Sumair-Ahmed <ahmednahiyo21@gmail.com>
2024-09-29 11:48:20 +0500

    i

---
+++ src/components/ScrollToTop/ScrollToTop.jsx
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from "react";
+import "./ScrollToTop.css";
+
+const ScrollToTop = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  // Show button when page is scrolled down
+  const toggleVisibility = () => {
+    if (window.scrollY > 300) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth", // smooth scroll to top
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", toggleVisibility);
+
+    return () => {
+      window.removeEventListener("scroll", toggleVisibility);
+    };
+  }, []);
+
+  return (
+    <div>
+      {isVisible && (
+        <button onClick={scrollToTop} className="scrollToTopButton">
+          ↑
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default ScrollToTop;
